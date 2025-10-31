71a3c19b0ad9cefc1eaabe306acdd07677edae26
Parth Sarthi Mishra <90957276+mrsarthi@users.noreply.github.com>
2025-10-31 11:58:02 +0530

    clean data

---
+++ app.py
@@ -0,0 +1,8 @@
+def askForMood():
+    mood = input('What are we in mood for today? ')
+
+    if mood == 'horror':
+        print('The Conjuring would be a great choice right about now')
+
+
+askForMood()
