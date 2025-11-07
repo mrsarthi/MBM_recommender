04f6debe0a3dd9dee47a228790bea73769c3738c
Parth Sarthi Mishra <wforsarthi_biness@outlook.com>
2025-11-07 12:07:49 +0530

    data preparation logic

--- dataAcquisition.py
+++ dataAcquisition.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 from dotenv import load_dotenv
 import os
 import re
@@ -8,23 +9,39 @@ load_dotenv()
 key = os.getenv('TMDB_key')
 baseUrl = "https://api.themoviedb.org/3"
 
-smartHeaders = {
-    "movie_id": None, "title": None, "summary": None, "user_rating": None, "tag": None, "with_whom": None, "after_feel": None
-}
+user_data = pd.read_csv('dataset/ratings.csv')
 
-headers = list(smartHeaders.keys())
+# def tmdbDataCollection():
+    
 
-df = pd.DataFrame(columns=headers)
+def migrate():
+    newData = pd.read_csv('dataset/V2ModelTrain.csv')
+    newData['title'] = user_data['Name']
+    newData['year'] = user_data['Year']
+    newData['user_rating'] = user_data['Rating']
 
-df.to_csv('dataset/V2ModelTrain.csv', index=False)
+    newData.to_csv('dataset/V2ModelTrain.csv', index=False)
 
-print(f"Created '{'dataset/V2ModelTrain.csv'}' with your headers.")
+def createCSV():
+    smartHeaders = {
+        "movie_id": None, "title": None, "year": None, "summary": None, "user_rating": None, "tag": None, "with_whom": None, "after_feel": None
+    }
 
-params = {
-    "api_key": key
-}
+    headers = list(smartHeaders.keys())
+    df = pd.DataFrame(columns=headers)
+    df.to_csv('dataset/V2ModelTrain.csv', index=False)
+
+    print(f"Created '{'dataset/V2ModelTrain.csv'}' with your headers.")
+
 
 def titleNormalize(title):
     title = title.lower()
     title = re.sub(r'[^a-z0-9]', '', title)
     return title
+
+
+if __name__ == "__main__":
+    createCSV()
+    migrate()
+    # tmdbDataCollection()
+    
